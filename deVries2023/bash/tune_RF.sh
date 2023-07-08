6aceb635347e5e5a32aa61c0603687ef80759a8e
nanophyto <coralscaper@gmail.com>
2023-07-08 17:31:55 +0100

    bash scripts to run with slurm

---
+++ deVries2023/bash/tune_RF.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#
+#SBATCH --time=0-12:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=1000M
+#SBATCH --cpus-per-task=16
+#SBATCH --array=0-41
+#SBATCH --account=GEOG024542
+
+i=${SLURM_ARRAY_TASK_ID}
+
+module  load apps/singularity/1.1.3 lib/openmpi/4.0.2-gcc.4.8.5 
+
+srun singularity exec \
+-B/user/work/$(whoami):/user/work/$(whoami) \
+/user/work/ba18321/planktonSDM/coccoML.sif \
+python /user/work/ba18321/planktonSDM/deVries2023/python/tune_RF.py ${SLURM_CPUS_PER_TASK} ${i} "1"
+
+export SINGULARITY_CACHEDIR=/user/work/$(whoami)/.singularity
