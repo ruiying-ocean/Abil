bcf24bc4bc8d3216fc90d7ebf620c9f8bd3d2be2
nanophyto <coralscaper@gmail.com>
2023-07-26 12:13:22 +0100

    updated to be more general

--- deVries2023/bash/tune_KNN.sh
+++ deVries2023/bash/tune_KNN.sh
@@ -3,18 +3,19 @@
 #
 #SBATCH --time=0-6:00:00
 #SBATCH --nodes=1
-#SBATCH --mem=6000M
+#SBATCH --mem=10000M
 #SBATCH --cpus-per-task=16
 #SBATCH --array=0-41
 #SBATCH --account=GEOG024542
 
 i=${SLURM_ARRAY_TASK_ID}
+path=${/user/work/ba18321/planktonSDM/}
 
 module  load apps/singularity/1.1.3 lib/openmpi/4.0.2-gcc.4.8.5 
 
 srun singularity exec \
 -B/user/work/$(whoami):/user/work/$(whoami) \
-/user/work/ba18321/planktonSDM/coccoML.sif \
-python /user/work/ba18321/planktonSDM/deVries2023/python/tune_KNN.py ${SLURM_CPUS_PER_TASK} ${i} "1"
+/user/work/$(whoami)/planktonSDM/planktonSDM.sif \
+python /user/work/$(whoami)/planktonSDM/deVries2023/python/tune_KNN.py ${SLURM_CPUS_PER_TASK} ${i} ${path}
 
 export SINGULARITY_CACHEDIR=/user/work/$(whoami)/.singularity
