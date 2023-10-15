d837880d2557e963660020f6a589d5695d27983e
nanophyto <coralscaper@gmail.com>
2023-10-15 20:43:10 +0100

    scp dir structure fix

--- to-bp.sh
+++ to-bp.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 #make a clean temp folder
-mkdir /tmp/ttt
-#copy folder but exclude git and dis
-rsync -av --progress /home/phyto/planktonSDM/ /tmp/ttt --exclude .git --exclude dist 
+mkdir /tmp/planktonSDM
+#copy folder but exclude git, docs and dis
+rsync -av --progress /home/phyto/planktonSDM/ /tmp/planktonSDM/ --exclude .git --exclude dist --exclude docs
 #copy temp folder to bp
-scp -r /tmp/ttt ba18321@bp1-login.acrc.bris.ac.uk:/user/work/ba18321/
+scp -r /tmp/planktonSDM/ ba18321@bp1-login.acrc.bris.ac.uk:/user/work/ba18321/
 #if copy ok, remove temp folder
-rm -rf /tmp/ttt
+rm -rf /tmp/planktonSDM
