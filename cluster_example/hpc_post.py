94741aa0b76b4e183433009b36a6616e56c0cb97
nanophyto <coralscaper@gmail.com>
2023-10-20 18:55:46 +0100

    fixes

--- cluster_example/hpc_post.py
+++ cluster_example/hpc_post.py
@@ -3,27 +3,32 @@ import sys
 from yaml import load
 from yaml import CLoader as Loader
 from planktonsdm.post import post
+import pandas as pd
+
 
 try:
     print(sys.argv[0])
     with open('/user/work/ba18321/planktonSDM/configuration/2-phase_ensemble_cluster.yml', 'r') as f:
         model_config = load(f, Loader=Loader)
     model_config['hpc'] = True
+    root = model_config['hpc_root']
 
 except:
     with open('/home/phyto/planktonSDM/configuration/2-phase_ensemble_cluster.yml', 'r') as f:
         model_config = load(f, Loader=Loader)
     model_config['hpc'] = False
+    root = model_config['local_root']
+
+
+X_predict =  pd.read_csv(root + model_config['env_data_path'])
+X_predict.set_index(["time", "depth", "lat", "lon"], inplace=True)
 
 
 m = post(model_config)
 m.merge_performance(model="ens")
 m.merge_performance(model="xgb", configuration= "reg")
-m.merge_performance(model="xgb", configuration= "zir")
 m.merge_performance(model="rf", configuration= "reg")
 m.merge_performance(model="knn", configuration= "reg")
-m.merge_performance(model="rf", configuration= "zir")
-m.merge_performance(model="knn", configuration= "zir")
 
 m.merge_parameters(model="rf")
 m.merge_parameters(model="xgb")
@@ -31,7 +36,7 @@ m.merge_parameters(model="knn")
 
 m.total()
 
-m.merge_env()
+m.merge_env(X_predict)
 
 m.export_ds("17_oct")
 m.export_csv("17_oct")
--- cluster_example/hpc_predict.py
+++ cluster_example/hpc_predict.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import OneHotEncoder
 
 try:
     print(sys.argv[1])
-    with open('/user/work/ba18321/planktonSDM/configuration/2-phase_ensemble_cluster.yml', 'r') as f:
+    with open('/user/work/ba18321/planktonSDM/configuration/ensemble_regressor_cluster.yml', 'r') as f:
         model_config = load(f, Loader=Loader)
     model_config['hpc'] = True
     n_jobs = pd.to_numeric(sys.argv[1])
@@ -18,7 +18,7 @@ try:
     model_config['cv'] = 10
 
 except:
-    with open('/user/work/ba18321/planktonSDM/configuration/2-phase_ensemble_cluster.yml', 'r') as f:
+    with open('/user/work/ba18321/planktonSDM/configuration/ensemble_regressor_cluster.yml', 'r') as f:
         model_config = load(f, Loader=Loader)
     model_config['hpc'] = False
     n_jobs = 8
