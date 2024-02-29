a6fb15319c517a1687ed451878332093ba6b323e
nanophyto <coralscaper@gmail.com>
2024-02-29 10:39:10 +0000

    export mapie during hpc post processing

--- devries2024/hpc_post.py
+++ devries2024/hpc_post.py
@@ -5,16 +5,19 @@ from yaml import CLoader as Loader
 from planktonsdm.post import post
 import pandas as pd
 
+from datetime import datetime
+current_date = datetime.today().strftime('%Y-%m-%d')
+
 
 try:
     print(sys.argv[0])
-    with open('/user/work/ba18321/planktonSDM/devries2024/2-phase_ensemble_deVries2024.yml', 'r') as f:
+    with open('/user/work/ba18321/planktonSDM/devries2024/ensemble_regressor_deVries2024.yml', 'r') as f:
         model_config = load(f, Loader=Loader)
     model_config['hpc'] = True
     root = model_config['hpc_root']
 
 except:
-    with open('/home/phyto/planktonSDM/devries2024/2-phase_ensemble_deVries2024.yml', 'r') as f:
+    with open('/home/phyto/planktonSDM/devries2024/ensemble_regressor_deVries2024.yml', 'r') as f:
         model_config = load(f, Loader=Loader)
     model_config['hpc'] = False
     root = model_config['local_root']
@@ -29,14 +32,23 @@ m.merge_performance(model="ens")
 m.merge_performance(model="xgb", configuration= "reg")
 m.merge_performance(model="rf", configuration= "reg")
 m.merge_performance(model="knn", configuration= "reg")
-
 m.merge_parameters(model="rf")
 m.merge_parameters(model="xgb")
 m.merge_parameters(model="knn")
-
 m.total()
-
 m.merge_env(X_predict)
+m.export_ds(current_date + "_ci50")
+m.export_csv(current_date + "_ci50")
 
-m.export_ds("17_oct")
-m.export_csv("17_oct")
+m = post(model_config, ci=32)
+m.total()
+m.merge_env(X_predict)
+m.export_ds(current_date + "_ci32")
+m.export_csv(current_date + "_ci32")
+
+m = post(model_config, ci=68)
+m.total()
+m.merge_env(X_predict)
+m.export_ds(current_date + "_ci68")
+m.export_csv(current_date + "_ci68")
+
