d3dd93e08d71b9dc2e209493ca6cc2335594d9d5
nanophyto <coralscaper@gmail.com>
2024-02-29 10:38:39 +0000

    omit ModelOutput folder from bash transfer to cluster

--- to-bp.sh
+++ to-bp.sh
@@ -3,7 +3,7 @@
 #make a clean temp folder
 mkdir /tmp/planktonSDM
 #copy folder but exclude git, docs and dis
-rsync -av --progress /home/phyto/planktonSDM/ /tmp/planktonSDM/ --exclude .git --exclude dist --exclude docs
+rsync -av --progress /home/phyto/planktonSDM/ /tmp/planktonSDM/ --exclude .git --exclude dist --exclude docs --exclude ModelOutput
 #copy temp folder to bp
 scp -r /tmp/planktonSDM/ ba18321@bp1-login.acrc.bris.ac.uk:/user/work/ba18321/
 #if copy ok, remove temp folder
