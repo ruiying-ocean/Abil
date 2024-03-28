6dac359bbf3cd2817ced7f9a23b5d95ab97aac92
nanophyto <coralscaper@gmail.com>
2024-03-28 14:06:21 +0000

    add pic and poc to hpc post processing

--- devries2024/hpc_post.py
+++ devries2024/hpc_post.py
@@ -11,13 +11,13 @@ current_date = datetime.today().strftime('%Y-%m-%d')
 
 try:
     print(sys.argv[0])
-    with open('/user/work/ba18321/Abil/devries2024/ensemble_regressor_deVries2024.yml', 'r') as f:
+    with open('/user/work/ba18321/Abil/devries2024/2-phase.yml', 'r') as f:
         model_config = load(f, Loader=Loader)
     model_config['hpc'] = True
     root = model_config['hpc_root']
 
 except:
-    with open('/home/phyto/Abil/devries2024/ensemble_regressor_deVries2024.yml', 'r') as f:
+    with open('/home/phyto/Abil/devries2024/2-phase.yml', 'r') as f:
         model_config = load(f, Loader=Loader)
     model_config['hpc'] = False
     root = model_config['local_root']
@@ -37,18 +37,61 @@ m.merge_parameters(model="xgb")
 m.merge_parameters(model="knn")
 m.total()
 m.merge_env(X_predict)
-m.export_ds(current_date + "_ci50")
-m.export_csv(current_date + "_ci50")
+m.export_ds(current_date + "_abundance_ci50")
+m.export_csv(current_date + "_abundance_ci50")
+
+
+m.estimate_carbon("POC")
+m.export_ds(current_date + "_POC_ci50")
+m.export_csv(current_date + "_POC_ci50")
+
+
+m = post(model_config)
+m.total()
+m.merge_env(X_predict)
+m.estimate_carbon("PIC")
+m.export_ds(current_date + "_PIC_ci50")
+m.export_csv(current_date + "_PIC_ci50")
+
+
+
+
 
 m = post(model_config, ci=32)
 m.total()
 m.merge_env(X_predict)
-m.export_ds(current_date + "_ci32")
-m.export_csv(current_date + "_ci32")
+m.export_ds(current_date + "_abundance_ci32")
+m.export_csv(current_date + "_abundance_ci32")
+
+m.estimate_carbon("POC")
+m.export_ds(current_date + "_POC_ci32")
+m.export_csv(current_date + "_POC_ci32")
+
+m = post(model_config, ci=32)
+m.total()
+m.merge_env(X_predict)
+m.estimate_carbon("PIC")
+m.export_ds(current_date + "_PIC_ci32")
+m.export_csv(current_date + "_PIC_ci32")
+
+
+
 
 m = post(model_config, ci=68)
 m.total()
 m.merge_env(X_predict)
-m.export_ds(current_date + "_ci68")
-m.export_csv(current_date + "_ci68")
 
+m.export_ds(current_date + "_abundance_ci68")
+m.export_csv(current_date + "_abundance_ci68")
+
+
+m.estimate_carbon("POC")
+m.export_ds(current_date + "_POC_ci68")
+m.export_csv(current_date + "_POC_ci68")
+
+m = post(model_config, ci=68)
+m.total()
+m.merge_env(X_predict)
+m.estimate_carbon("PIC")
+m.export_ds(current_date + "_PIC_ci68")
+m.export_csv(current_date + "_PIC_ci68")
