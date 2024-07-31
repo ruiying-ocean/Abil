a20a02a5c1985e752c79c39fc35b8754e27b77eb
Nicola Wiseman <nicola.wiseman@bristol.ac.uk>
2024-07-31 14:10:52 +0100

    change to-bp for bluecrystal

--- to-bp.sh
+++ to-bp.sh
@@ -3,8 +3,8 @@
 #make a clean temp folder
 mkdir /tmp/Abil
 #copy folder but exclude git, docs and dis
-rsync -av --progress /home/phyto/Abil/ /tmp/Abil/ --exclude .git --exclude dist --exclude docs --exclude ModelOutput
+rsync -av --progress /home/mv23682/Documents/Abil/ /tmp/Abil/ --exclude .git --exclude dist --exclude docs --exclude studies/wiseman2024/ModelOutput --exclude studies/wiseman2024/data/preprocessing_data/Misc
 #copy temp folder to bp
-scp -r /tmp/Abil/ ba18321@bp1-login.acrc.bris.ac.uk:/user/work/ba18321/
+scp -r /tmp/Abil/ mv23682@bc4login.acrc.bris.ac.uk:/user/work/mv23682/
 #if copy ok, remove temp folder
 rm -rf /tmp/Abil
