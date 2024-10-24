bc399f9d0e37cc71cb3ca4e5d33aad068f81a280
Nicola Wiseman <nicola.wiseman@bristol.ac.uk>
2024-10-24 14:06:39 +0100

    add raw_data preprocessing, and env data merging to studies/wiseman/env_data_processing Ignore folders with data to converse space, these will be put in a repository

---
+++ studies/wiseman2024/env_data_processing/merge_WOA_po4.py
@@ -0,0 +1,61 @@
+#%%
+# Merge monthly WOA18 Phosphate and subset to 0-200m
+
+import numpy as np
+import xarray as xr
+import xesmf as xe
+import pandas as pd
+
+base_path = '/home/mv23682/Documents/Abil-1/studies/wiseman2024/env_data_processing/raw_data/WOA18/'
+files = ['01',
+         '02',
+         '03',
+         '04',
+         '05',
+         '06',
+         '07',
+         '08',
+         '09',
+         '10',
+         '11',
+         '12']
+ds_all = []
+for i in range(0, 12):
+    ds = xr.open_dataset(base_path + "woa18_all_p" + files[i] + "_01.nc",
+                         decode_times=False,drop_variables={'climatology_bounds','crs','depth_bnds',
+                                                            'p_dd','p_gp','p_ma','p_mn','p_oa','p_sd','p_se',
+                                                            'lat_bnds','lon_bnds','time'}
+                         )
+    df = ds.to_dataframe()
+    df.reset_index(inplace=True)
+    df = df[df["depth"] <= 200]
+    print(df)
+    df = df.set_index(['depth','lat','lon'])
+    ds = df.to_xarray()
+    ds = ds.interp(depth=np.arange(0,205,5))
+    ds_out = xr.Dataset({'lat': (['lat'], np.arange(-90, 90, 1)),
+                    'lon': (['lon'], np.arange(-180, 180, 1)),
+                    'depth': (['depth'], np.arange(0, 205, 5))
+                    })
+    regridder1 = xe.Regridder(ds, ds_out, method="bilinear", periodic=True)
+    dr_out = regridder1(ds['p_an'])
+    df = dr_out.to_dataframe(name="po4")
+    df.reset_index(inplace = True)
+    df['time'] = i+1
+    df = df.groupby(['time', 'depth', 'lat', 'lon']).mean().reset_index()
+    df.set_index(['time', 'depth', 'lat', 'lon'], inplace=True)
+    print(df)
+    ds = df.to_xarray()
+    ds_all.append(ds)
+
+ds = xr.merge(ds_all)
+df = ds.to_dataframe()
+df.reset_index(inplace = True)
+df.set_index(['time', 'depth', 'lat', 'lon'], inplace=True)
+ds = df.to_xarray()
+
+ds.to_netcdf("/home/mv23682/Documents/Abil-1/studies/wiseman2024/env_data_processing/regridded_data/po4.nc")
+
+print("fin")
+
+# %%
