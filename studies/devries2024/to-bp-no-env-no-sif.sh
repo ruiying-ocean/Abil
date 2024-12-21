0c5546085b2cabf88cf16228cc83f8c5e3e56f3b
nanophyto <coralscaper@gmail.com>
2024-12-21 22:33:09 +0000

    fixes

--- studies/devries2024/to-bp-no-env-no-sif.sh
+++ studies/devries2024/to-bp-no-env-no-sif.sh
@@ -4,7 +4,7 @@
 mkdir /tmp/Abil
 #copy folder but exclude git, docs and dis
 rsync -av --progress /home/phyto-2/Abil/ /tmp/Abil/ --exclude .git --exclude dist \
---exclude docs --exclude ModelOutput --exclude examples --exclude tests
+--exclude docs --exclude ModelOutput --exclude examples --exclude tests --exclude studies/devries2024/data --exclude '*.sif'
 #copy temp folder to bp
 scp -r /tmp/Abil/ ba18321@bp1-login.acrc.bris.ac.uk:/user/work/ba18321/
 #if copy ok, remove temp folder
--- studies/devries2024/tune_KNN.sh
+++ studies/devries2024/tune_KNN.sh
@@ -4,17 +4,17 @@
 #SBATCH --time=0-12:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=10000M
-#SBATCH --cpus-per-task=16
-#SBATCH --array=0-51
+#SBATCH --cpus-per-task=32
+#SBATCH --array=0-38
 #SBATCH --account=GEOG024542
 
 i=${SLURM_ARRAY_TASK_ID}
 
-module  load apps/singularity/1.1.3 lib/openmpi/4.0.2-gcc.4.8.5 
+module  load apptainer/1.3.1 
 
-srun singularity exec \
+singularity exec \
 -B/user/work/$(whoami):/user/work/$(whoami) \
-/user/work/$(whoami)/Abil/singularity/abil.sif \
-python /user/work/$(whoami)/Abil/devries2024/hpc_tune.py ${SLURM_CPUS_PER_TASK} ${i} "knn"
+/user/work/$(whoami)/Abil/studies/devries2024/abil.sif \
+python /user/work/$(whoami)/Abil/studies/devries2024/hpc_tune.py ${SLURM_CPUS_PER_TASK} ${i} "knn"
 
 export SINGULARITY_CACHEDIR=/user/work/$(whoami)/.singularity
