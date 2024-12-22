3ee5265cf1b10f0c98d6d10d0c58cb88f6c90c1a
nanophyto <coralscaper@gmail.com>
2024-12-22 12:15:19 +0000

    predict fixes

--- studies/devries2024/hpc_predict.py
+++ studies/devries2024/hpc_predict.py
@@ -1,44 +1,32 @@
 # import required packages
 import pandas as pd
-import numpy as np
 import sys
 from yaml import load
 from yaml import CLoader as Loader
 from abil.predict import predict
-from abil.functions import upsample, OffsetGammaConformityScore
-from sklearn.preprocessing import OneHotEncoder
+from abil.functions import upsample
 
-try:
-    print(sys.argv[1])
-    with open('/user/work/ba18321/Abil/studies/devries2024/2-phase.yml', 'r') as f:
-        model_config = load(f, Loader=Loader)
-    model_config['hpc'] = True
-    n_jobs = pd.to_numeric(sys.argv[1])
-    n_spp = pd.to_numeric(sys.argv[2])
-    root = model_config['root']
-    model_config['cv'] = 10
-
-except:
-    with open('/user/work/ba18321/Abil/studie/devries2024/2-phase.yml', 'r') as f:
-        model_config = load(f, Loader=Loader)
-    model_config['hpc'] = False
-    n_jobs = 1
-    n_spp = 1
-    root = model_config['root']
-    model_config['cv'] = 3
+with open('/user/work/ba18321/Abil/studies/devries2024/2-phase.yml', 'r') as f:
+    model_config = load(f, Loader=Loader)
+model_config['hpc'] = True
+n_jobs = pd.to_numeric(sys.argv[1])
+n_spp = pd.to_numeric(sys.argv[2])
+root = model_config['root']
+model_config['cv'] = 10
 
 #define model config:
 model_config['n_threads'] = n_jobs
 targets = pd.read_csv(root + model_config['targets'])
 d = pd.read_csv(root + model_config['training'])
 target =  targets['Target'][n_spp]
+predictors = model_config['predictors']
 d[target] = d[target].fillna(0)
 d = d.dropna(subset=[target])
+d = d.dropna(subset=predictors)
 d = upsample(d, target, ratio=10)
 print(target)
-predictors = model_config['predictors']
 
-X_predict =  pd.read_csv(root + model_config['env_data_path'])
+X_predict =  pd.read_csv(root + model_config['prediction'])
 X_predict.set_index(["time", "depth", "lat", "lon"], inplace=True)
 y = d[target]
 X_train = d[predictors]
